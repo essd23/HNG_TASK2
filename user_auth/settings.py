77cfb5f6a5a3430eca44f5d3f89d1bade87cc3b9
EssD <akinyemisaed@gmail.com>
2024-07-09 22:48:13 +0100

    commit finito..

--- user_auth/settings.py
+++ user_auth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wkv=s!p(sbs*=qg0taw=_q0bmy08x1u_=xp_=mimivb3&z1b#@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '*']
 
 
 # Application definition
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'user_auth.urls'
