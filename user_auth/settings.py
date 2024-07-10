514f561dd870272eedfe58317fcb90cbee564148
EssD <akinyemisaed@gmail.com>
2024-07-10 20:48:35 +0100

    commit finito..

--- user_auth/settings.py
+++ user_auth/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
